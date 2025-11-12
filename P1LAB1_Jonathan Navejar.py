0b2532cba0d55883d0fc4b565ba7741d797860ef
JonyNavatar <navejarj5340@student.faytechcc.edu>
2025-11-12 15:20:51 -0500

    Add files via upload

---
+++ P1LAB1_Jonathan Navejar.py
@@ -0,0 +1,8 @@
+#Jonathan H. Navejar
+#November 12. 2025
+#p1lab1.py
+#input and print statements
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello,",first_name,last_name +"!","Welcome to CTI-110!")
